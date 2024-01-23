09e58c2ecec1b83b11c2b1bf11060fcb2cef83e0
Shruthi M <111205197+shru0405@users.noreply.github.com>
2024-01-23 03:01:19 -0500

    Update App.js

--- React_page/App.js
+++ React_page/App.js
@@ -11,3 +11,18 @@ function App() {
 }
 
 export default App;
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import RegistrationForm from './components/registrationForm'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
